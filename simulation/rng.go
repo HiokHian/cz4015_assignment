05350044634d3fef688c5b62c57534aaba57cc63
HiokHian <Ohiokhian@gmail.com>
2023-01-30 19:04:08 +0800

    'rng'

--- simulation/rng.go
+++ simulation/rng.go
@@ -44,20 +44,3 @@ func (rng NormalDistribution) sample() float64 {
     return z * rng.std + rng.mean // to change to larger sample size
 }
 
-// func (r rect) perim() float64 {
-//     return 2*r.width + 2*r.height
-// }
-
-// func (c circle) area() float64 {
-//     return math.Pi * c.radius * c.radius
-// }
-// func (c circle) perim() float64 {
-//     return 2 * math.Pi * c.radius
-// }
-
-// func measure(g geometry) {
-//     fmt.Println(g)
-//     fmt.Println(g.area())
-//     fmt.Println(g.perim())
-// }
-
